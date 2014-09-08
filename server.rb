ce7d72749a736a2dc9721f20208cced1bdb504b2
Jerome Pratt <jerome.pratt@outlook.com>
2014-09-08 18:29:40 +0100

    create user with email and password, changed layout...done by vincent(kikrahau), who is super cool

--- server.rb
+++ server.rb
@@ -5,12 +5,16 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
 require './lib/tag'
+require './lib/user'
 
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
 
 # set :views, Proc.new{ File.join(root, 'views') } 
+
+enable :sessions
+set :session_secret, 'super secret'
 
 get '/' do
 	@links = Link.all
@@ -32,4 +36,28 @@ get '/tags/:text' do
 	@links = tag ? tag.links : []
 	erb :index
 end
+
+get '/users/new' do 
+	erb :"users/new"
+end
+
+post '/users' do 
+	user = User.create(:email => params[:email],
+						:password => params[:password])
+	session[:user_id] = user.id
+	redirect to('/')
+end
+
+helpers do
+
+	def current_user
+		@current_user ||=User.get(session[:user_id]) if session[:user_id]
+	end
+
+end
+
+
+
+
+
 
