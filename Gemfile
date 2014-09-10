9196f531e66834f772da079e4d51b8e473f7d6ae
Jerome Pratt <jerome.pratt@outlook.com>
2014-09-10 12:04:20 +0100

    refactored flash errors and made it so an email can only be registered once

--- Gemfile
+++ Gemfile
@@ -1,13 +1,13 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 gem 'sinatra'
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
-gem 'database_cleaner'
-gem 'capybara'
 gem 'bcrypt-ruby'
 gem 'rack-flash3'
 
 group :development, :test do
 	gem 'rspec'
+	gem 'database_cleaner'
+	gem 'capybara'
 end
--- server.rb
+++ server.rb
@@ -1,7 +1,9 @@
 require 'data_mapper'
 require 'sinatra'
 require 'rack-flash'
+
 env = ENV["RACK_ENV"] || "development"
+
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
@@ -12,62 +14,62 @@ DataMapper.finalize
 
 DataMapper.auto_upgrade!
 
-# set :views, Proc.new{ File.join(root, 'views') } 
+class BookmarkManager < Sinatra::Base
 
-enable :sessions
-set :session_secret, 'super secret'
-use Rack::Flash
+	enable :sessions
+	set :session_secret, 'super secret'
+	
+	use Rack::Flash
+	use Rack::MethodOverride
 
-get '/' do
-	@links = Link.all
-	erb :index
-end
-
-post '/links' do
-	url = params["url"]
-	title = params["title"]
-	tags = params["tags"].split(" ").map do |tag|
-		Tag.first_or_create(:text => tag)
+	get '/' do
+		@links = Link.all
+		erb :index
 	end
-	Link.create(:url => url, :title => title, :tags => tags)
-	redirect to('/')
-end
 
-get '/tags/:text' do 
-	tag = Tag.first(:text => params[:text])
-	@links = tag ? tag.links : []
-	erb :index
-end
-
-get '/users/new' do 
-	@user = User.new
-	erb :"users/new"
-end
-
-post '/users' do 
-	@user = User.new(:email => params[:email],
-						:password => params[:password],
-						:password_confirmation => params[:password_confirmation])
-	if @user.save
-		session[:user_id] = @user.id 
+	post '/links' do
+		url = params["url"]
+		title = params["title"]
+		tags = params["tags"].split(" ").map do |tag|
+			Tag.first_or_create(:text => tag)
+		end
+		Link.create(:url => url, :title => title, :tags => tags)
 		redirect to('/')
-	else
-		flash[:notice] = "Sorry, your passwords don't match"
+	end
+
+	get '/tags/:text' do 
+		tag = Tag.first(:text => params[:text])
+		@links = tag ? tag.links : []
+		erb :index
+	end
+
+	get '/users/new' do 
+		@user = User.new
 		erb :"users/new"
 	end
-end
+
+	post '/users' do 
+		@user = User.new(:email => params[:email],
+							:password => params[:password],
+							:password_confirmation => params[:password_confirmation])
+		if @user.save
+			session[:user_id] = @user.id 
+			redirect to('/')
+		else
+			flash.now[:errors] = @user.errors.full_messages
+			erb :"users/new"
+		end
+	end
 
 
 
-helpers do
-
-	def current_user
-		@current_user ||=User.get(session[:user_id]) if session[:user_id]
+	helpers do
+		def current_user
+			@current_user ||=User.get(session[:user_id]) if session[:user_id]
+		end
 	end
 
 end
-
-
 
 
 
